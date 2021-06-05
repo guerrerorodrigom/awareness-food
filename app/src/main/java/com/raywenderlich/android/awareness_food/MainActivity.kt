922b5cf40c5134e5d2114d666b69a06cab3479e5
Rodrigo Guerrero <warrior.software.minds@gmail.com>
2021-06-05 08:47:18 +0200

    Add ui loading state code.

--- app/src/main/java/com/raywenderlich/android/awareness_food/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/awareness_food/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
@@ -52,6 +53,7 @@ import com.raywenderlich.android.awareness_food.monitor.NetworkMonitor
 import com.raywenderlich.android.awareness_food.monitor.NetworkState
 import com.raywenderlich.android.awareness_food.repositories.models.RecipeApiState
 import com.raywenderlich.android.awareness_food.viewmodels.MainViewModel
+import com.raywenderlich.android.awareness_food.viewmodels.UiLoadingState
 import com.raywenderlich.android.awareness_food.views.IngredientView
 import com.squareup.picasso.Picasso
 import dagger.android.AndroidInjection
@@ -171,5 +173,15 @@ class MainActivity : AppCompatActivity() {
           view.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
           show()
         }
+  }
+
+  private fun handleLoadingState(uiLoadingState: UiLoadingState?) {
+    when (uiLoadingState) {
+      UiLoadingState.Loading -> {
+        clearViews()
+        binding.progressBar.isVisible = true
+      }
+      UiLoadingState.NotLoading -> binding.progressBar.isVisible = false
+    }
   }
 }
